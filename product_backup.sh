58f1aca193ac7f9dffec763405006e381c9599bb
Akshay Pradeep <akshay.p.pradheep@gmail.com>
2021-11-18 13:28:21 +0530

    change in flow scp just after the tar and remove file which can save space

--- product_backup.sh
+++ product_backup.sh
@@ -1,52 +1,205 @@
 #!/bin/bash
+process_count_check(){
+	P_COUNT=`ps -ef|grep $0|grep -v grep|wc -l|awk '{print $1}'`
+	if [ $P_COUNT -ge 4 ]; then
+			echo `date +"%Y%m%d %H:%M:%S"` [ERROR] $0 already running $P_COUNT
+			exit
+	fi
+}
 
-echo `date +"%Y%m%d %H:%M:%S"` [INFO] Backup Script stared
+read_config(){
+
+	configuration_file=$1
+	cat $configuration_file |grep = > /tmp/backup_script_source.tmp
+	source /tmp/backup_script_source.tmp
+}
+
+
+check_size_dir_disk (){
+        current_mount=`findmnt -T .|awk 'NR==2{print $1}'`
+        available_space=`df |grep -w "$current_mount" |awk '{print $4}'`
+        dir=`echo $1|awk -F '^' '{print $1}'`
+        exclude_filesr=`echo $1|awk -F '^' '{print $2}'`
+        max=$2
+
+                if [ -d $dir ]
+                then
+                        if [ -z $exclude_files ]
+                        then
+                                dir_size=$(du -c $dir|awk ' END{print $1}')
+                        else
+                                dir_size=$(du -c $dir `echo $exclude_files|tr ',' '\n' |xargs -i echo "--exclude={}"|xargs echo` |awk ' END{print $1}')
+                        fi
+                else
+                        echo `date +"%Y%m%d %H:%M:%S"` [ERROR] $dir Not exist/Not a Directory
+                        exit 1
+                fi
+                if [ $available_space -lt $margin ]
+                then
+                        echo "`date +"%Y%m%d %H:%M:%S"` [ERROR] Space available in the current volum is less than expected DIR size x margin:$margin  avaliable: $available_space"
+			exit
+                fi
+
+
+}
+
+
+
+present_working_mount_disk_check(){
+	current_mount=`findmnt -T .|awk 'NR==2{print $1}'`
+	available_space=`df |grep -w "$current_mount" |awk '{print $4}'`
+	disk_usage_percentage=`df |grep -w "$current_mount" |awk '{print $5}'|tr -d '%'`
+	mode=$1
+	threshold=$2
+	case $mode in
+				"p")
+					if [ $disk_usage_percentage -gt $threshold ]
+					then
+						echo `date +"%Y%m%d %H:%M:%S"` [ERROR] "$current_mount" Disk Usage is ${disk_usage_percentage}%, inorder to start the scrip disk usage shouldnt be more than ${threshold}%
+						exit 2
+					fi
+					;;
+				"s")
+					if [ $available_space -lt $threshold ]
+					then
+						echo `date +"%Y%m%d %H:%M:%S"` [ERROR] In "$current_mount" avaliable space is ${available_space}B inorder to start the script min ${threshold}B should be avaliable
+						exit 2
+					fi
+					;;
+				"c")
+					echo "$current_mount ${available_space}K ${disk_usage_percentage}%"
+	esac
+}
+
+
+compress_dir (){
+        tar_name=$1
+	dir=$2
+        exclude_files=$3
+        parent_dir=`echo "$dir"|awk -F',' '{print $1"/../"}'`
+        child_dir=`echo "$dir"|awk -F',' '{print $1}'|tr '/' ' '|awk '{print $NF}'`
+        tar_name_tmp=`echo "$dir"|awk -F',' '{print $2}'`
+		if [ -d $dir ]
+		then
+			echo `date +"%Y%m%d %H:%M:%S"` [INFO] started compressing $dir
+			if [ -z $exclude_files ]
+			then
+					tar -zcf $tar_name -C $parent_dir $child_dir
+					echo `date +"%Y%m%d %H:%M:%S"` [INFO] finished compressing $parent_dir/$child_dir
+			else
+					tar -zcf $tar_name `echo $exclude_files|tr ',' '\n' |xargs -i echo "--exclude={}"|xargs echo` -C $parent_dir $child_dir
+					echo `date +"%Y%m%d %H:%M:%S"` [INFO] finished compressing $parent_dir/$child_dir excluding dirs $exclude_files
+			fi
+		else
+			echo `date +"%Y%m%d %H:%M:%S"` [ERROR] $dir Not exist/Not a Directory
+			exit 1
+		fi
+
+}
+
+transfer_file(){
+	OPTIND=0
+	OPTARG=0
+        while getopts "t:f:h:u:p:d:r:" arg ;do
+                case $arg in
+                t)
+                        transfer_type=$OPTARG
+                        ;;
+                f)
+                        file=$OPTARG
+                        ;;
+                h)
+                        host=$OPTARG
+                        ;;
+                u)
+                        username=$OPTARG
+                        ;;
+                p)
+                        password=$OPTARG
+                        ;;
+                d)
+                        destination=$OPTARG
+                        ;;
+                r)
+                        rm_flag=$OPTARG
+                        ;;
+                esac
+        done
+  if [[ -z $transfer_type || -z $file || -z $host || -z $username || -z $destination ]]
+        then
+                echo `date +"%Y%m%d %H:%M:%S"` [ERROR] Please Provide Nessessory Params
+        else
+                case $transfer_type in
+                                "scp") scp -q $file $username@$host:$destination
+						rc=$?
+                                                if [ $rc -eq 0 ]
+                                                then
+                                                        echo `date +"%Y%m%d %H:%M:%S"` [SUCCESS] Sucessfully SCPed $file to $username@$host
+                                                        if [ $rm_flag = y ]
+                                                        then
+                                                                echo `date +"%Y%m%d %H:%M:%S"` [INFO] Removing $file
+                                                                rm $file
+                                                        fi
+                                                else
+                                                        echo `date +"%Y%m%d %H:%M:%S"` [ERROR] Failed to scp $file to $username@$host with error code $rc
+                                                        exit 1
+                                                fi
+                                        ;;
+                esac
+        fi
+
+}
+
+
+
+
+
+#################################################################################
+
+
+>/tmp/backup_script_scp_list.tmp
 
 configuration_file=/opt/akshay/etc/config/product_backup.cfg
-temp_dir=`grep -w temp_dir $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-dwh_flag=`grep -w dwh $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-transfer_type=`grep -w transfer_type $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-dwh_hostip=`grep -w dwh_hostip $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-dwh_user=`grep -w dwh_user $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-dwh_password=`grep -w dwh_password $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-dwh_dest_path=`grep -w dwh_dest_path $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-delete_after_scp=`grep -w delete_after_scp $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-naming=`grep -w naming $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-disk_usage_check=`grep -w disk_usage_check $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-disk_usage_threshold=`grep -w disk_usage_threshold $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-disk_usage_check_loop=`grep -w disk_usage_check_loop $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
-disk_usage_check_loop_threshold=`grep -w disk_usage_check_loop_threshold $configuration_file|grep -v '#'|awk -F '=' '{print $NF}'`
+read_config $configuration_file
 
-
->/tmp/backup_script.tmp
->/tmp/backup_script_scp_list.tmp
 cd $temp_dir
-current_mount=`findmnt -T .|awk 'NR==2{print $1}'`
-available_space=`df |grep -w $current_mount |awk '{print $4}'`
-disk_usage_percentage=`df |grep -w "$current_mount" |awk '{print $5}'|tr -d '%'`
-
-if [ $disk_usage_threshold -gt $available_space  ]
+if [ $? -ne 0 ]
 then
-	echo `date +"%Y%m%d %H:%M:%S"` [ERROR] No Space in the current mount exiting the script
-	exit 2
-else
-	echo `date +"%Y%m%d %H:%M:%S"` [INFO] Space Check is fine 
+        echo `date +"%Y%m%d %H:%M:%S"` [ERROR] wrong temp dir
+        exit 2
 fi
-P_COUNT=`ps -ef|grep $0|grep -v grep|wc -l|awk '{print $1}'`
-if [ $P_COUNT -ge 4 ]; then
-        echo "`date` $0 already running ($P_COUNT)"
-        exit
+if [ $disk_usage_check = y ]
+then
+	if [ $disk_usage_check_mode -eq 1 ]
+	then
+		present_working_mount_disk_check p $disk_usage_check_percentage
+	fi
+	if [ $disk_usage_check_mode -eq 2  ]
+        then
+                present_working_mount_disk_check s $min_available_space
+        fi
+
 fi
 
-cat $configuration_file|grep -v '#' |grep -v  '='|grep -v "^$"|while read line
+
+cat $configuration_file|grep -v '#'|grep -v '=' |grep -v -e '^$' |while read line
 do
-	date_cron=`date +'%M %H %d %m %u'`
-	config_cron=`echo "$line"|cut -d':' -f1`
 	cron_flag=0
-	for i in {1..5}
+	date_cron=`date +'%M %H %d %m %u'`
+	config_cron=`echo "$line" |cut -d':' -f1`
+	dir_ap=`echo "$line" |cut -d':' -f2|cut -d, -f1`
+	exclude_dirs=`echo "$line" |cut -d':' -f3`
+	tar_name_temp=`echo "$line" |cut -d':' -f2|cut -d, -f2`
+	tar_name_ap=${tar_name_temp}_`${naming}`.tar.gz
+
+	for i in {1..5} #cron correct aakanam
 	do
 		date_cron_ap="`echo "$date_cron" |cut -d' ' -f${i}`"
 		config_cron_ap="`echo "$config_cron" |cut -d' ' -f${i}`"
+		re='^[0-9]+$';if  [[ $config_cron_ap =~ $re ]] ;
+		then
+			mode='n'
+		else
 		echo "$config_cron_ap"|grep ',' >> /dev/null
 		if [ $? -eq 0  ]
 		then 
@@ -64,8 +217,14 @@ do
                 then
                         mode='*'
                 fi
-
+		fi
 		case $mode in 
+			'n')
+				if [ $date_cron_ap -eq $config_cron_ap ]
+				then
+					cron_flag=`expr $cron_flag + 1 `
+				fi
+				;;
 			"*")
 				cron_flag=`expr $cron_flag + 1 `
 				;;
@@ -85,100 +244,38 @@ do
 				fi
 		esac
 
+		#echo "$date_cron_ap : $config_cron_ap : $cron_flag : $mode"
 	done
 	if [ $cron_flag -eq 5  ]
 	then
-		echo "$line" >> /tmp/backup_script.tmp
+		echo $tar_name_ap $dir_ap $exclude_dirs  >> /tmp/backup_script_scp_list.tmp
 		echo `date +"%Y%m%d %H:%M:%S"` [INFO] adding "$line" to exicution queue
 	fi
-
 done
 
-if [ `cat /tmp/backup_script.tmp|wc -l` -eq 0 ]
+if [ `cat /tmp/backup_script_scp_list.tmp|wc -l` -eq 0 ]
 then
-	echo `date +"%Y%m%d %H:%M:%S"` [INFO] No files in queue Exiting script
-	exit 
+        #echo `date +"%Y%m%d %H:%M:%S"` [INFO] No files in queue Exiting script
+        exit
 fi
 
-cd $temp_dir
-cat /tmp/backup_script.tmp|while read line
+
+cat /tmp/backup_script_scp_list.tmp|while read line
 do
-
-	tar_cfg=`echo "$line"|awk -F':' '{print $2}'`
-	parent_dir=`echo "$tar_cfg"|awk -F',' '{print $1"/../"}'`
-	child_dir=`echo "$tar_cfg"|awk -F',' '{print $1}'|tr '/' ' '|awk '{print $NF}'`
-	exclude_files=`echo "$line" | awk -F':' '{print $3}' `
-	tar_name_tmp=`echo "$tar_cfg"|awk -F',' '{print $2}'`
-	tar_name=${tar_name_tmp}_`$naming`.tar.gz
-	if [ $disk_usage_check_loop = 'y'  ]
+ 	
+	if [ $folder_size_check = y  ]
 	then
-		if [ $disk_usage_check_loop_threshold -lt  $disk_usage_percentage ]
+		check_size_dir_disk `echo $line|cut -d' ' -f2-|tr ' ' '^'` $margin
+	fi
+
+	compress_dir `echo $line`
+		if [ $dwh_flag = y ] 
 		then
-			echo `date +"%Y%m%d %H:%M:%S"` [ERROR] Disk Space Exceed the threshould current percentage:$disk_usage_percentage configured:$disk_usage_check_loop_threshold
-			exit 
-
+			if [ $delete_after_scp = y ] 
+			then
+				transfer_file -t $transfer_type -f "$temp_dir/$tar_name" -h "$dwh_hostip" -u "$dwh_user" -d "$dwh_dest_path" -r "y"
+			else
+				transfer_file -t $transfer_type -f "$temp_dir/$tar_name" -h "$dwh_hostip" -u "$dwh_user" -d "$dwh_dest_path" -r "n"
+			fi
 		fi
-	fi
-
-	if [ -z $exclude_files ]
-	then
-		echo `date +"%Y%m%d %H:%M:%S"` [INFO] started compressing $parent_dir/$child_dir
-		tar -zcf $tar_name -C $parent_dir $child_dir
-		echo `date +"%Y%m%d %H:%M:%S"` [INFO] finished compressing $parent_dir/$child_dir
-	else
-		echo `date +"%Y%m%d %H:%M:%S"` [INFO] started compressing $parent_dir/$child_dir excluding dirs $exclude_files
-		tar -zcf $tar_name `echo $exclude_files|tr ',' '\n' |xargs -i echo "--exclude={}"|xargs echo` -C $parent_dir $child_dir
-		echo `date +"%Y%m%d %H:%M:%S"` [INFO] finished compressing $parent_dir/$child_dir excluding dirs $exclude_files
-	fi
-	echo ${temp_dir}/$tar_name >> /tmp/backup_script_scp_list.tmp
 done
-
-if [ $dwh_flag = y ]
-then
-	echo `date +"%Y%m%d %H:%M:%S"` [INFO] DWH ENABLED
-	case $transfer_type in 
-		"ftp")
-			echo `date +"%Y%m%d %H:%M:%S"` [INFO] Transfer type  FTP
-			cat /tmp/backup_script_scp_list.tmp|while read file
-			do
-				exitStat=$(curl -v --ftp-method singlecwd  -T $file ftp://$dwh_user:$dwh_password@$dwh_hostip"/$dwh_dest_path/$file")
-				if [ $? -eq 0 ]
-				then
-					echo `date +"%Y%m%d %H:%M:%S"` [INFO] Succesfully FTPED $file to DWH server $dwh_user@$dwh_hostip
-				else
-					echo `date +"%Y%m%d %H:%M:%S"` [ERROR]  Failed FTP $file to DWH server $dwh_user@$dwh_hostip 
-				fi
-			done
-			;;
-		"scp")
-			echo `date +"%Y%m%d %H:%M:%S"` [INFO] Transfer type  SCP
-			cat /tmp/backup_script_scp_list.tmp|while read file
-                        do
-				scp $file $dwh_user@$dwh_hostip:$dwh_dest_path >> /dev/null
-				if [ $? -eq 0 ]
-                                then
-					echo `date +"%Y%m%d %H:%M:%S"` [INFO] Succesfully SCPED $file to DWH server $dwh_user@$dwh_hostip:$dwh_dest_path
-				else
-					echo `date +"%Y%m%d %H:%M:%S"` [ERROR]  Failed SCP $file to DWH server $dwh_user@$dwh_hostip
-				fi
-			done
-	esac
-else
-	echo `date +"%Y%m%d %H:%M:%S"` [INFO] DWH DISABLED
-
-fi
-
-if [ $delete_after_scp = y ]
-then
-	if [ `cat /tmp/backup_script_scp_list.tmp|wc -l` -eq 0  ]
-	then
-		echo `date +"%Y%m%d %H:%M:%S"` [ERROR] no files to delete
-		exit
-	fi
-
-	echo `date +"%Y%m%d %H:%M:%S"` [INFO] delete_after_scp enabled removing the scped tar files 
-	cat /tmp/backup_script_scp_list.tmp|xargs rm 
-else
-	echo `date +"%Y%m%d %H:%M:%S"` [INFO] delete_after_scp disabled keeping the scped tar files	
-fi
-echo `date +"%Y%m%d %H:%M:%S"` [INFO] Backup Script Finished
